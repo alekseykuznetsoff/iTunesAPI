fa1904e590c7ae9ab443d02a36853bc333dd29bc
Kuznetsov Aleksey <aleksey.d.kuznetsov@gmail.com>
2018-05-15 09:22:32 +0300

    added list controller wip

---
+++ iTunesAPI/UI/List/WFMListTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  WFMListTableViewController.h
+//  iTunesAPI
+//
+//  Created by Kuznetsov Aleksey on 15.05.2018.
+//  Copyright © 2018 Kuznetsov Aleksey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WFMListTableViewController : UITableViewController
+
+@end
